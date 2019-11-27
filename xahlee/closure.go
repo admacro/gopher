d6860d5d1a6d15d6342e4d7e9bc2a4fc8d1f713c
James Ni <admacro@gmail.com>
2019-11-28 00:34:15 +0800

    closure

---
+++ xahlee/closure.go
@@ -0,0 +1,47 @@
+// http://xahlee.info/golang/golang_closure.html
+package main
+
+import "fmt"
+
+// Golang's function is a closure.
+// Closure is a programming language feature. It is a function such
+// that all variables it uses will still work outside where it is defined.
+
+// For example:
+//   1. a function f defines a local variable a and a local function g
+//   2. a is used in g
+//   3. f returns g
+//   4. f is called and result is assigned to h
+//   5. h is now a function that is g. all variables used by h, including a
+//  still work, even though they were local variables of f
+//   6. h is called a closure
+
+// In practice, closure means you can create a function that maintains a state,
+// without using global variable.
+
+// You create a closure by defining a function that returns a function. Then
+// call the function and assign the result to a variable. The variable is a new
+// function, a closure.
+
+// Closure is basically the core concept of "class and object" in object-oriented
+// programming languages, like Java, Python, Ruby, etc.
+
+func main() {
+	var class = func(x int) (getter func() int, setter func(x int)) {
+		var a = x
+		getter = func() int {
+			return a
+		}
+		setter = func(x int) {
+			a = x
+		}
+		return getter, setter
+	}
+
+	var get, set = class(1)
+	fmt.Printf("get(): %v\n", get())	// get(): 1
+
+	fmt.Println("set(3)")
+	set(3)
+	fmt.Printf("get(): %v\n", get())	// get(): 3
+}
