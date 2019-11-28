397a9152c954969aaf913382eaa2a4db8a44592f
James Ni <admacro@gmail.com>
2019-11-28 15:55:13 +0800

    more char sequence

--- xahlee/char_sequence.go
+++ xahlee/char_sequence.go
@@ -2,6 +2,7 @@
 package main
 
 import "fmt"
+import "unicode/utf8"
 
 // char sequence in golang can be represented in 3 different formats
 //   1. string (immutable byte sequence)
@@ -56,7 +57,7 @@ func main() {
 
 	var rs = []rune(s)
 	pf("runes: %v\n", rs)
-	pf("runes: %c\n", rs)
+	pf("runes: %q\n", rs)
 
 	// decimal, hexdecimal, and char value of ♥
 	pf("runes[6]: dec(%v) hex(%x) char(%c)\n", rs[6], rs[6], rs[6])
@@ -67,7 +68,7 @@ func main() {
 	// rune slice ==> string
 	pl("\n# rune slice ==> string ----------")
 	pf("runes: %v\n", rs)
-	pf("runes: %c\n", rs)
+	pf("runes: %q\n", rs)
 
 	s = string(rs)
 	pf("string: %v\n", s)
@@ -80,7 +81,7 @@ func main() {
 	// first convert byte slice to string, then covert string to rune slice
 	rs = []rune(string(bs))
 	pf("runes: %v\n", rs)
-	pf("runes: %c\n", rs)
+	pf("runes: %q\n", rs)
 
 	// decimal, hexdecimal, and char value of ♥
 	pf("runes[6]: dec(%v) hex(%x) char(%c)\n", rs[6], rs[6], rs[6])
@@ -101,4 +102,67 @@ func main() {
 	pf("bytes[6]: dec(%v) hex(%x) char(%c)\n", bs[6], bs[6], bs[6])
 
 	pf("type of bytes: %T\n", bs)	// []int32
+
+
+	// number of characters (runes)
+	pl("\n# number of characters ----------")
+	pf("char count of string: %v (using utf8.RuneCount([]byte(s)))\n", utf8.RuneCount([]byte(s)))
+	pf("char count of string: %v (using utf8.RuneCountInString)\n", utf8.RuneCountInString(s))
+	pf("char count of byte slice: %v\n", utf8.RuneCount(bs))
+	pf("char count of rune slice: %v\n", len(rs))
+
+
+	// substring by character index
+	pl("\n# substring by character index ----------")
+	rs = []rune(s)
+	pf("%v\n", rs[5:7])
+	pf("%q\n", rs[5:7])
+
+
+	// given byte index that starts a char, find the index of the char
+	pl("\n# find char index by byte index ----------")
+	var i = 9 // index of the first byte of 好 in string s
+
+	pf("index of 好 in s: %v\n", utf8.RuneCountInString(s[0:i]))
+	pf("index of 好 in rs: %v\n", utf8.RuneCount(bs[0:i]))
+
+
+	// given random byte index, find the index that starts a char to
+	// which the random byte belongs
+	pl("\n# find char index by random byte index ----------")
+	var charIndex = func(bs []byte, i int) int {
+		for j := i; j >= 0; j-- {
+			if utf8.RuneStart(bs[j]) {
+				return utf8.RuneCount(bs[0:j])
+			}
+		}
+		return 0
+	}
+	pf("random byte index in byte slice: %v, char index: %v\n", 7, charIndex(bs, 7)) // 6 ♥
+	pf("random byte index in byte slice: %v, char index: %v\n", 9, charIndex(bs, 9)) // 7 好
+	pf("random byte index in byte slice: %v, char index: %v\n", 15, charIndex(bs, 15)) // 8 🀄
+
+	var charIndexInString = func(s string, i int) int {
+		for j := i; j >= 0; j-- {
+			if utf8.RuneStart(s[j]) {
+				return utf8.RuneCountInString(s[0:j])
+			}
+		}
+		return 0
+	}
+	pf("random byte index in string: %v, char index: %v\n", 7, charIndexInString(s, 7)) // 6 ♥
+	pf("random byte index in string: %v, char index: %v\n", 9, charIndexInString(s, 9)) // 7 好
+	pf("random byte index in string: %v, char index: %v\n", 15, charIndexInString(s, 15)) // 8 🀄
+
+
+	pl("\n# loop through characters in string ----------")
+
+	// i is index (with respect to byte, not character)
+	// c is character (which is rune in golang, its value is an integer)
+	for i, c := range s {
+		pf("s[%v] = %q (%v) (%U) (%T)\n", i, c, c, c, c)
+		// s[6] = '♥' (9829) (U+2665) (int32)
+		// s[9] = '好' (22909) (U+597D) (int32)
+		// s[12] = '🀄' (126980) (U+1F004) (int32)
+	}
 }
