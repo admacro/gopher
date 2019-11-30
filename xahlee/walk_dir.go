5e48934309fbb9ee29a3b255e70fdf79a506af98
James Ni <admacro@gmail.com>
2019-11-30 19:34:39 +0800

    add more comment explaining Walk and walkFunc

--- xahlee/walk_dir.go
+++ xahlee/walk_dir.go
@@ -7,6 +7,11 @@ import "os"
 
 func main() {
 
+	// path is root/file.ext
+	//   root is the first arg of filepath.Walk
+	// Hence, if Walk is called with "abc", and xyz is the file name, then
+	// "abc/xyz" is passed to path when walkFn (fn) is called, and info is
+	// the os.FileInfo for the named path
 	var fn = func(path string, info os.FileInfo, err error) error {
 		// first check if there is any error
 		if err != nil {
@@ -40,6 +45,8 @@ func main() {
 	fmt.Printf("%v %5v bytes %40v %10v %10v %v\n",
 		"Permission", "Size", "Modification Time",
 		"Extention", "Directory", "Name")
+
+	// func Walk(root string, walkFn WalkFunc) error
 	err := filepath.Walk(dir, fn)
 	if err != nil {
 		fmt.Printf("Error walking filepath %v\n", err, dir)
