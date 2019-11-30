3a67ad6ad052e5da78f99c75552de7ee3a34b082
James Ni <admacro@gmail.com>
2019-11-30 20:02:02 +0800

    check file exist

---
+++ xahlee/check_file_exist.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+import "os"
+
+func fileExist(path string) bool {
+	// according to
+	//   https://groups.google.com/forum/#!msg/golang-nuts/Ayx-BMNdMFo/4rL8FFHr8v4J
+	// you shouldn't check file existence beforehand, because if you want to do something about
+	// the file, you will need to open the file. And the file could disappear between checking
+	// and opening. So you simply call os.IsNotExist(err) after you open the file and deal
+	// with the err there.
+
+	// Using os.Stat to determine whether a path exist or not
+	// this is easy enough for the cases where it is required
+	_, err := os.Stat(path)
+	if err != nil && os.IsNotExist(err) {
+		fmt.Printf("File %v does not exist\n", path)
+		return false
+	}
+	fmt.Printf("File %v exist\n", path)
+	return true
+}
+
+func main() {
+	fileExist("pointer.go")
+	fileExist("/Users/james/Downloads/Titanic.mp4")
+}
