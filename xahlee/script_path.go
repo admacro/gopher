3cb0d08d99f91a1f339ee9fd4b0d5cfe727a655a
James Ni <admacro@gmail.com>
2019-12-01 11:16:55 +0800

    script (binary executable file) path

---
+++ xahlee/script_path.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+import "os"
+
+func main() {
+	path, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+
+	// for scripting languages, script is the code file itself
+	// for static languages, it's a compiled binary executable file
+	// /var/folders/sp/n3hrxcgd3wgb8zb_3mt1w9kr0000gn/T/go-build931736641/b001/exe/script_path
+	fmt.Println(path)
+}
