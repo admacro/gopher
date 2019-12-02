c569fe664377d2f221c368efc5b5a3b86f7a0c72
James Ni <admacro@gmail.com>
2019-12-02 14:59:55 +0800

    more on rand.Seed

--- xahlee/random2.go
+++ xahlee/random2.go
@@ -4,7 +4,8 @@ import "fmt"
 import "math/rand"
 
 func main() {
-	// make the same calls as in random.go in different order
+	// make the same calls as in random.go, but in different order
+	// compare output with random.go
 	for i := 0; i < 30; i++ {
 		fmt.Printf("%v", rand.Intn(4))
 	} // 133312100023210231123202333023
@@ -12,6 +13,6 @@ func main() {
 	fmt.Println()
 
 	fmt.Printf("rand.Intn(180) => %v\n", rand.Intn(180))	 // 121
-	fmt.Printf("rand.Intn(5000) => %v\n", rand.Intn(5000)) // 508
+	fmt.Printf("rand.Intn(5000) => %v\n", rand.Intn(5000)) // 408
 	fmt.Printf("rand.Intn(10) => %v\n", rand.Intn(10))		 // 7
 }
--- xahlee/random_seed.go
+++ xahlee/random_seed.go
@@ -8,16 +8,30 @@ func main() {
 	// Seed uses the provided seed value to initialize the default Source
 	// to a deterministic state.
 	// If Seed is not called, the generator behaves as if seeded by Seed(1).
-	// Seed values that have the same remainder when divided by 2^31-1 generate
-	// the same pseudo-random sequence.
 
-	var seed int64 = 29						// type of seed is int64
+	var seed int64 = 111						// type of seed is int64
 	rand.Seed(seed)
 	fmt.Printf("Set seed to %v\n", seed)
-	fmt.Printf("rand.Intn(10) => %v\n", rand.Intn(10))		 // 3
-	fmt.Printf("rand.Intn(180) => %v\n", rand.Intn(180))	 // 36
-	fmt.Printf("rand.Intn(5000) => %v\n", rand.Intn(5000)) // 4450
+	fmt.Printf("rand.Intn(10) => %v\n", rand.Intn(10))		 // 0
+	fmt.Printf("rand.Intn(180) => %v\n", rand.Intn(180))	 // 80
+	fmt.Printf("rand.Intn(5000) => %v\n", rand.Intn(5000)) // 2420
 	for i := 0; i < 30; i++ {
 		fmt.Printf("%v", rand.Intn(4))
-	} // 102003022300333212232000211300
+	} // 201300121330020120220100021311
+
+	// Seed values that have the same remainder when divided by 2^31-1 generate
+	// the same pseudo-random sequence.
+	// Note: `^` in `2^31 - 1` is not the bitwise operator XOR in a programming
+	// context, but the power sign in a mathematical context, thus `2^31 - 1` is
+	// not 28, but 2147483647
+	// see details here: https://github.com/golang/go/issues/35920
+	seed = 2147483647 + 111
+	rand.Seed(seed)
+	fmt.Printf("\n\nSet seed to %v\n", seed)
+	fmt.Printf("rand.Intn(10) => %v\n", rand.Intn(10))		 // 0
+	fmt.Printf("rand.Intn(180) => %v\n", rand.Intn(180))	 // 80
+	fmt.Printf("rand.Intn(5000) => %v\n", rand.Intn(5000)) // 2420
+	for i := 0; i < 30; i++ {
+		fmt.Printf("%v", rand.Intn(4))
+	} // 201300121330020120220100021311
 }
