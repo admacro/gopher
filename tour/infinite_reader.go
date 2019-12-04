6f7b095fa1e8b091c82569b28988b8621edc8171
James Ni <admacro@gmail.com>
2019-12-05 00:05:37 +0800

    reader exercise: inifinite A emitter

---
+++ tour/infinite_reader.go
@@ -0,0 +1,26 @@
+// http://tour.golang.org/methods/22
+package main
+
+import "fmt"
+
+type MyReader struct {}
+
+// emits an infinite stream of the ASCII character 'A'
+func (r MyReader) Read(b []byte) (n int, err error) {
+	var a = "A"
+	for i, _ := range b {
+		b[i] = a[0]
+		n++
+	}
+	return n, nil
+}
+
+func main() {
+	var reader MyReader
+	b := make([]byte, 8)
+	for i := 0; i < 10; i++ {
+		n, err := reader.Read(b)
+		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
+		fmt.Printf("b[:n] = %q\n", b[:n])
+	}
+}
