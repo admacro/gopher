66234431a23adeabc8d5f45d92c01c4d4aa03d12
James Ni <admacro@gmail.com>
2019-12-06 19:47:52 +0800

    close channel and test channel status

---
+++ tour/channel_close.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+// A sender can close a channel to indicate that no more values will be sent.
+// Only the sender should close a channel, never the receiver.
+// Channels aren't like files; you don't usually need to close them.
+// Closing is only necessary when the receiver must be told there are no more
+// values coming, such as to terminate a range loop.
+func fibonacci(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
+	}
+	close(c)
+}
+
+func main() {
+	c := make(chan int, 10)
+	go fibonacci(cap(c), c)
+	// receives values from the channel repeatedly until it is closed.
+	for i := range c {
+		fmt.Println(i)
+	}
+
+	// panic: send on closed channel
+	// go fibonacci(cap(c), c)
+
+	// Receivers can test whether a channel has been closed by assigning a second
+	// parameter to the receive expression.
+	//   v, ok := <-ch
+	// ok is false if there are no more values to receive and the channel is closed.
+	ch := make(chan int, 5)
+	go fibonacci(cap(ch), ch)
+	for {
+		v, ok := <-ch
+		if ok {
+			fmt.Println(v)
+		} else {
+			fmt.Printf("Channel %#v is closed.\n", ch)
+			break
+		}
+	}
+}
