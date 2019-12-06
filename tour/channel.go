1c7e82f1492f392bd537ac93c4646c6e5a47313c
James Ni <admacro@gmail.com>
2019-12-06 17:46:20 +0800

    channel and more goroutine

---
+++ tour/channel.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+// Channels are a typed conduit through which you can send and receive values
+// with the channel operator: <-
+// The data flows in the direction of the arrow.
+
+// You create channels using make:
+//   make(chan Type)
+//   Type is the type of the value you can send and receive with the channel
+
+// By default, sends (c <- v) and receive (v <-c) blocks until the other side is
+// ready. This allows goroutines to synchronize without explicit locks or
+// condition variables.
+
+func sum(n []int, c chan int) {
+	sum := 0
+	for _, v := range n {
+		sum += v
+	}
+	c <- sum // send value sum to channel c
+}
+
+func main() {
+	n := generateRandomNumbers()
+
+	c := make(chan int)
+	go sum(n[:len(n)/2], c)
+	go sum(n[len(n)/2:], c)
+
+	x, y := <-c, <-c // receive value from c and assign to x and y
+	fmt.Println(x, y, x+y)
+
+	// Error when all goroutines have completed their work
+	// fatal error: all goroutines are asleep - deadlock!
+	// x = <-c
+}
+
+func generateRandomNumbers() (n [100]int) {
+	for i := range n {
+		n[i] = rand.Intn(100)
+	}
+	return
+}
--- tour/goroutine.go
+++ tour/goroutine.go
@@ -5,13 +5,24 @@ import (
 	"time"
 )
 
+// A Go routine is a lightweight thread managed by Go runtime.
+// go f(x, y, z) starts a new goroutine running f(x, y, z).
+
+// The evaluation of f(x, y, z) happens in the current goroutine and the
+// execution of f(x, y, z) happens in the new goroutine.
+
+// Goroutines run in the same address space, so access to shared memory must be
+// synchronized. The `sync` package provides useful primitives, although you
+// don't need them much in Go as there is better choices (channel).
+
 func sayHi(from string, to string) {
 	fmt.Printf("%v: Hi %v!\n", from, to)
 }
 
 func main() {
 	newKid := "Tom"
-	fmt.Printf("Mrs. Helen: boys and girls, this is %v. Can you say hi to %v?\n", newKid, newKid)
+	fmt.Printf("Mrs. Helen: boys and girls, this is %v. Can you say hi to %v?\n",
+		newKid, newKid)
 	kids := []string{"Jack", "Emma", "Johnny", "Cindy"}
 	for _, kid := range kids {
 		go sayHi(kid, newKid)
