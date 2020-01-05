b36d652bb6049c69a19aa82e89c966f784142909
James Ni <admacro@gmail.com>
2020-01-05 16:57:50 +0800

    refine goroutine example

--- tour/goroutine.go
+++ tour/goroutine.go
@@ -15,7 +15,9 @@ import (
 // synchronized. The `sync` package provides useful primitives, although you
 // don't need them much in Go as there is better choices (channel).
 
-func sayHi(from string, to string) {
+func sayHi(from string, to string, i int) {
+	// time.Millisecond is of type time.Duration
+	time.Sleep(time.Duration(200 * i) * time.Millisecond)
 	fmt.Printf("%v: Hi %v!\n", from, to)
 }
 
@@ -24,18 +26,18 @@ func main() {
 	fmt.Printf("Mrs. Helen: boys and girls, this is %v. Can you say hi to %v?\n",
 		newKid, newKid)
 	kids := []string{"Jack", "Emma", "Johnny", "Cindy"}
-	for _, kid := range kids {
-		go sayHi(kid, newKid)
+	for i, kid := range kids {
+		go sayHi(kid, newKid, i)
 	}
 
-	// hardcoded waiting time (3s)
-	// This waiting is necessary. Because in this case the main() almost always
-	// exits before the goroutines ever start, and you will be able to see output
-	// from the goroutines.
+	// hardcoded waiting time (1s)
+	// This waiting is necessary. Gorotunes are terminated when main() exists.
+	// In this case the main() almost always exits before the goroutines ever start,
+	// and you won't be able to see any output from the goroutines without explicit waiting.
+	time.Sleep(1000 * time.Millisecond)
 
-	// Better (Correct) ways to wait for all goroutines to complete before the
-	// main program exits are:
+	// Yet, there are better (or correct) ways to wait for all goroutines to complete
+	// before the main program exits, such as:
 	//   1. sync.WaitGroup
 	//   2. channel
-	time.Sleep(1000 * time.Millisecond)
 }
