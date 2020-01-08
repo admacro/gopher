5898aa0eebc8e8afd3132e39efbbbf583d23ad67
James Ni <admacro@gmail.com>
2020-01-08 16:22:45 +0800

    hahahaha. Can't help add more concurrent stuff in advance lol

--- tour/channel.go
+++ tour/channel.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 )
 
 // Channels are a typed conduit through which you can send and receive values
@@ -20,34 +21,62 @@ import (
 // P.S.
 // gofmt favors the following formats when formatting the channel operator:
 //   when sending value to channel (space at both sides of <-): c <- v
-//   when receiving value from channel (space at the value side, not the channel side): v <-c
+//   when receiving value from channel (space at the value side, not the channel side): v := <-c
 
-func sum(n []int, c chan int) {
+var counter = 0
+var m sync.Mutex
+var wg sync.WaitGroup
+
+func sum(n []int, c chan int, sumId int) {
+	defer wg.Done()
+
+	fmt.Printf("[%d] Summing %d numbers: %v\n", sumId, len(n), n)
 	sum := 0
 	for _, v := range n {
 		sum += v
 	}
+
 	c <- sum // send value sum to channel c
+
+	// guard critical section
+	m.Lock()
+	counter = counter + 1
+	fmt.Printf("[%d] Number of value sent to channel: %v, value: %v\n", sumId, counter, sum)
+	m.Unlock()
 }
 
 func main() {
-	n := generateRandomNumbers()
+	size := 100
+	n := generateRandomNumbers(size)
 
 	c := make(chan int)
-	go sum(n[:len(n)/2], c)
-	go sum(n[len(n)/2:], c)
+	step := 10
 
-	x, y := <-c, <-c // receive value from c and assign to x and y
-	fmt.Println(x, y, x+y)
+	sumId := 1
+	for i := range n {
+		if i%step == 0 {
+			start := i
+			end := i + step
+			go sum(n[start:end], c, sumId)
+			sumId++
+			wg.Add(1)
+		}
+	}
 
-	// Error when all goroutines have completed their work
-	// fatal error: all goroutines are asleep - deadlock!
-	// x = <-c
+	sum := 0
+	for i := 0; i < size/step; i++ {
+		sum += <-c
+	}
+	wg.Wait()
+
+	fmt.Printf("%v numbers sent and received through channel\n", counter)
+	fmt.Printf("Sum of %v pseudorandom numbers: %v\n", size, sum)
 }
 
-func generateRandomNumbers() (n [100]int) {
-	for i := range n {
-		n[i] = rand.Intn(100)
+func generateRandomNumbers(size int) []int {
+	var n = make([]int, size)
+	for i := 0; i < size; i++ {
+		n[i] = rand.Intn(10)
 	}
-	return
+	return n
 }
