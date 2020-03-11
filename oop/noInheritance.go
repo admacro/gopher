118619fa68f798462e2add254d7a0e2bc20e78c2
James Ni <admacro@gmail.com>
2020-03-11 11:00:49 +0800

    more comments; interface

--- oop/noInheritance.go
+++ oop/noInheritance.go
@@ -15,25 +15,43 @@ func (a *Animal) Walk() {
 	fmt.Printf("%s walking...\n", a.name)
 }
 
-// noraml composition
-// you need to provide forwarding method to access the underlying method in Animal
+// noraml composition:
+//   - some code resue
+//   - you need to provide forwarding method (delegation) to access the underlying method in Animal
 type Cat struct {
 	animal Animal
 }
 
+// delegation
 func (c *Cat) Walk() {
 	fmt.Print("calling animal to walk... ")
 	c.animal.Walk()
 }
 
-// embedding
-//
+// composition and embedding: field and method reuse
 type Dog struct {
 	Animal
 }
 
-func (a *Dog) Walk() {
-	fmt.Printf("%s running...\n", a.name)
+// interface: polymorphism and dynamic dispatch
+type Sleeper interface {
+	Sleep()
+}
+
+type Human struct {
+	Animal
+}
+
+func (a *Animal) Sleep() {
+	fmt.Printf("%s sleeping...\n", a.name)
+}
+
+func (d *Dog) Sleep() {
+	fmt.Printf("%s sleeping...\n", d.name)
+}
+
+func (h *Human) Sleep() {
+	fmt.Printf("%s sleeping...\n", h.name)
 }
 
 func main() {
@@ -49,4 +67,25 @@ func main() {
 	// same effect as a composition `a Animal`
 	d := Dog{Animal: Animal{name: "Dog"}}
 	d.Walk()
+
+	h := Human{}
+	h.name = "Human"
+
+	// so interface can be seen as a means to group types by behaviour
+	// if
+	//     - A and B do x
+	//     - B and C do y
+	//     - interface Xer has method x
+	//     - interface Yer has method y
+	// then
+	//     - A and B are grouped as Xers
+	//     - Or, B and C are bother Yers
+	//
+	// When I see a bird that walks like a duck and swims like a duck and
+	// quacks like a duck, I call that bird a duck.
+	//                                              –James Whitcomb Riley
+	sleepers := []Sleeper{&d, &h}
+	for _, s := range sleepers {
+		s.Sleep()
+	}
 }
