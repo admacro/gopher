f8e6136b5f65c21eb49f62d0a099fea26a1c11b9
James Ni <admacro@gmail.com>
2020-03-24 16:52:02 +0800

    embedded fields and methods in struct

--- oop/embedded.go
+++ oop/embedded.go
@@ -1,51 +1,73 @@
+// https://golang.org/ref/spec#Struct_types
 package main
 
 import "fmt"
 
-type Car struct {
-	model string
+type T struct {
+	x, y int
 }
 
-func (c *Car) register() {
-	fmt.Println("start engine...")
+func (t *T) ReceivePointerT() {
+	fmt.Printf("ReceivePointer: %#v\n", t)
 }
 
-func (c Car) recycle() {
-	fmt.Println("recycle car...")
+func (t T) ReceiveValue() {
+	fmt.Printf("ReceiveValue: %#v\n", t)
 }
 
-type MedicalRecord struct {
-	vision     float32
-	impairment bool
+func (t T) IdenticalMethod() {
+	fmt.Printf("IdenticalMethod of T: %#v\n", t)
 }
 
-func (mi MedicalRecord) updateMedicalRecord() {
-	fmt.Printf("update medical info [vision: %f, impairment: %v]\n", mi.vision, mi.impairment)
+type Pt struct {
+	y, z int
 }
 
-func (mi *MedicalRecord) expireMedicalRecord() {
-	fmt.Printf("expire medical info [vision: %f, impairment: %v]\n", mi.vision, mi.impairment)
+func (pt Pt) ReceiveValuePt() {
+	fmt.Printf("ReceiveValuePt: %#v\n", pt)
 }
 
-type Driver struct {
-	*Car
-	MedicalRecord
+func (pt *Pt) ReceivePointer() {
+	fmt.Printf("ReceivePointer: %#v\n", pt)
+}
+
+func (pt Pt) IdenticalMethod() {
+	fmt.Printf("IdenticalMethod of Pt: %#v\n", pt)
+}
+
+type S struct {
+	// embedded fields
+	T   // embedded value type
+	*Pt // embedded pointer type (Pt must not be an interface type or a pointer type)
 }
 
 func main() {
-	driver := Driver{
-		MedicalRecord: MedicalRecord{
-			vision:     2.0,
-			impairment: false,
-		},
-		Car: &Car{
-			model: "Toyota RAV4",
-		},
+	s := &S{
+		Pt: &Pt{}, T: T{},
 	}
-	fmt.Printf("%#v\n", driver.Car)
-	driver.register()
-	driver.recycle()
-	driver.Car.register()
-	driver.updateMedicalRecord()
-	driver.expireMedicalRecord()
+	fmt.Printf("%#v\n", s) // main.S{T:main.T{x:0, y:0}, Pt:(*main.Pt)(0xc0000140a0)}
+
+	// A field or method f of an embedded field in a struct x is called promoted if
+	// x.f is a legal selector that denotes that field or method f.
+
+	// for f in x.f, if there is not exactly one f with shallowest depth, the selector expression is illegal.
+	// see selector rules #1 at https://golang.org/ref/spec#Selectors
+
+	// promoted fields
+	fmt.Printf("s.x = %v\n", s.x)
+	fmt.Printf("s.z = %v\n", s.z)
+
+	// fmt.Println(s.y) // error: ambiguous selector s.y
+	fmt.Printf("s.T.y = %v\n", s.T.y)
+	fmt.Printf("s.Pt.y = %v\n", s.Pt.y)
+
+	// promoted methods
+	s.ReceiveValue()
+	s.ReceiveValuePt()
+	s.ReceivePointer()
+	s.ReceivePointerT()
+
+	// s.IdenticalMethod() // error: ambiguous selector s.IdenticalMethod
+	s.T.IdenticalMethod()
+	s.Pt.IdenticalMethod()
 }
