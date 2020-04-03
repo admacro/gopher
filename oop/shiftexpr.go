2adf975f10a5a4ecaf1cb0a10ae0fc745735e2a6
James Ni <admacro@gmail.com>
2020-04-03 14:14:12 +0800

    fix compilation error

--- oop/shiftexpr.go
+++ oop/shiftexpr.go
@@ -13,38 +13,55 @@ import (
 )
 
 func main() {
-	var ii = 1.0<<33
-	fmt.Printf("%T: %v", ii, ii)
+	var ii = 1.0 << 33
+	fmt.Printf("%T: %v\n", ii, ii)
 
 	var s uint = 33
 
 	// determine the type of left operand of a non-constant shift expression
-	// 
+	//
 	// first 1<<s is a non-constant shift expression
 	// second, 1<<s is replaced by the left oprand of the shift expression, which is 1
 	// now the whoe statement becomes: var i = 1
 	// in the new statement, the type of 1 is the default type of constant 1, which is int
 	// now, go back to the shift expression, we can know the type of the left operand
 	// the type of it is int
-	var i = 1<<s                  // 1 has type int
+	var i = 1 << s // 1 has type int
+	fmt.Printf("%T: %v\n", i, i)
 
 	// apply the same analysis to the following statement, you will know the type of the
 	// left operand of the shift expression is int32, because after the replacement, the
 	// new statement is: var j int32 = 1
 	// thus the type of 1 is int32
-	var j int32 = 1<<s            // 1 has type int32; j == 0
+	var j int32 = 1 << s // 1 has type int32; j == 0
+	fmt.Printf("%T: %v\n", j, j)
 
 	// the same applies to the following statement
-	var k = uint64(1<<s)          // 1 has type uint64; k == 1<<33
-	var m int = 1.0<<s            // 1.0 has type int; m == 0 if ints are 32bits in size
-	var n = 1.0<<s == j           // 1.0 has type int32; n == true
-	var o = 1<<s == 2<<s          // 1 and 2 have type int; o == true if ints are 32bits in size
-	var p = 1<<s == 1<<33         // illegal if ints are 32bits in size: 1 has type int, but 1<<33 overflows int
-	var u = 1.0<<s                // illegal: 1.0 has type float64, cannot shift
-	var u1 = 1.0<<s != 0          // illegal: 1.0 has type float64, cannot shift
-	var u2 = 1<<s != 1.0          // illegal: 1 has type float64, cannot shift
-	var v float32 = 1<<s          // illegal: 1 has type float32, cannot shift
-	var w int64 = 1.0<<33         // 1.0<<33 is a constant shift expression
-	var x = a[1.0<<s]             // 1.0 has type int; x == a[0] if ints are 32bits in size
-	var a = make([]byte, 1.0<<s)  // 1.0 has type int; len(a) == 0 if ints are 32bits in size
+	var k = uint64(1 << s) // 1 has type uint64; k == 1<<33
+	var m int = 1.0 << s   // 1.0 has type int; m == 0 if ints are 32bits in size
+	var n = 1.0<<s == j    // 1.0 has type int32; n == true
+	var o = 1<<s == 2<<s   // 1 and 2 have type int; o == true if ints are 32bits in size
+	// var p = 1<<s == 1<<33        // illegal if ints are 32bits in size: 1 has type int, but 1<<33 overflows int
+	// var u = 1.0 << s             // illegal: 1.0 has type float64, cannot shift
+	// var u1 = 1.0<<s != 0         // illegal: 1.0 has type float64, cannot shift
+	// var u2 = 1<<s != 1.0         // illegal: 1 has type float64, cannot shift
+	// var v float32 = 1 << s       // illegal: 1 has type float32, cannot shift
+	var w int64 = 1.0 << 33      // 1.0<<33 is a constant shift expression
+	var a = make([]byte, 1.0<<s) // 1.0 has type int; len(a) == 0 if ints are 32bits in size
+	// var x = a[1.0<<s]            // 1.0 has type int; x == a[0] if ints are 32bits in size
+
+	fmt.Printf("%T: %v\n", k, k)
+	fmt.Printf("%T: %v\n", m, m)
+	fmt.Printf("%T: %v\n", n, n)
+	fmt.Printf("%T: %v\n", o, o)
+	// fmt.Printf("%T: %v\n", p, p)
+	// fmt.Printf("%T: %v\n", u, u)
+	// fmt.Printf("%T: %v\n", u1, u1)
+	// fmt.Printf("%T: %v\n", u2, u2)
+	// fmt.Printf("%T: %v\n", v, v)
+	fmt.Printf("%T: %v\n", w, w)
+	fmt.Printf("%T: %v\n", a, a)
+
+	// 1<<s overflows int
+	// fmt.Printf("%T: %v\n", x, x)
 }
