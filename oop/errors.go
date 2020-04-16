03f4b75ca350af01d92cf3e5198cf54590933266
James Ni <admacro@gmail.com>
2020-04-16 13:04:02 +0800

    errors

---
+++ oop/errors.go
@@ -0,0 +1,37 @@
+// https://golang.org/ref/spec#Errors
+// https://blog.golang.org/error-handling-and-go (12 July 2011)
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type InvalidZipcodeError struct {
+	Zipcode string
+}
+
+func (err InvalidZipcodeError) Error() string {
+	return fmt.Sprintf("Invalid zipcode: %s", err.Zipcode)
+}
+
+func main() {
+	// a very basic error that only has a string message
+	err := errors.New("something wrong happend")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// error message formatting
+	err = fmt.Errorf("something wrong happend at %v", time.Now())
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// custom error with more useful information
+	err = InvalidZipcodeError{"12345a"}
+	if err != nil {
+		fmt.Println(err)
+	}
+}
---
+++ oop/errorwrapping.go
@@ -0,0 +1,24 @@
+// https://golang.org/ref/spec#Errors
+// https://blog.golang.org/error-handling-and-go (12 July 2011)
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	// create wrapped errors
+	// apply %w verb to the underlying error
+	uerr := errors.New("underlying error")
+	err := fmt.Errorf("this error wraps [%w]", uerr)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// unwrap an error to get the underlying error
+	uerr1 := errors.Unwrap(err)
+	if uerr1 != nil {
+		fmt.Println(uerr1)
+	}
+}
