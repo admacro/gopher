499d89eb72b7639df58efc669fd064ac434e197f
James Ni <admacro@gmail.com>
2022-07-16 23:14:44 +0800

    learn go module

---
+++ tut/hello/hello.go
@@ -0,0 +1,13 @@
+// https://go.dev/doc/tutorial/getting-started
+// https://pkg.go.dev/rsc.io/quote/v4
+package main
+
+import "fmt"
+import "rsc.io/quote"
+
+func main() {
+    fmt.Println(quote.Hello())
+    fmt.Println(quote.Go())
+    fmt.Println(quote.Glass())
+    fmt.Println(quote.Opt())
+}
---
+++ tut/module/hello/hello.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "fmt"
+    "log"
+
+    "example.com/greetings"
+)
+
+func main() {
+	// Set properties of the predefined Logger
+	log.SetPrefix("greetigs: ")	// log entry prefix
+	log.SetFlags(0)				// do not print time, source file, and line number
+	
+    // Request greeting messages.
+	names := []string{"Gladys", "Emma", "Hare"}
+    messages, err := greetings.Hellos(names)
+
+	// If an error was returned, print it to the console and exit the program
+	if err != nil {
+		log.Fatal(err)			
+	}
+
+	// Print the messages to the console if no error was returned.
+    fmt.Println(messages)
+}
