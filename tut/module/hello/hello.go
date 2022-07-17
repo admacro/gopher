84a80b748de5379348791adbc7506f789807ec9b
James Ni <admacro@gmail.com>
2022-07-18 00:36:55 +0800

    reformat code using gofmt

--- tut/module/hello/hello.go
+++ tut/module/hello/hello.go
@@ -2,26 +2,26 @@
 package main
 
 import (
-    "fmt"
-    "log"
+	"fmt"
+	"log"
 
-    "example.com/greetings"
+	"example.com/greetings"
 )
 
 func main() {
 	// Set properties of the predefined Logger
-	log.SetPrefix("greetigs: ")	// log entry prefix
-	log.SetFlags(0)				// do not print time, source file, and line number
-	
-    // Request greeting messages.
+	log.SetPrefix("greetigs: ") // log entry prefix
+	log.SetFlags(0)             // do not print time, source file, and line number
+
+	// Request greeting messages.
 	names := []string{"Gladys", "Emma", "Hare"}
-    messages, err := greetings.Hellos(names)
+	messages, err := greetings.Hellos(names)
 
 	// If an error was returned, print it to the console and exit the program
 	if err != nil {
-		log.Fatal(err)			
+		log.Fatal(err)
 	}
 
 	// Print the messages to the console if no error was returned.
-    fmt.Println(messages)
+	fmt.Println(messages)
 }
--- tut/web-service-gin/main.go
+++ tut/web-service-gin/main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"log"
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,17 +13,17 @@ import (
 // when the struct’s contents are serialized into JSON. Without them, the JSON
 // would use the struct’s capitalized field names – a style not as common in JSON.
 type album struct {
-    ID     string  `json:"id"`
-    Title  string  `json:"title"`
-    Artist string  `json:"artist"`
-    Price  float64 `json:"price"`
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
 }
 
 // albums slice to seed record album data.
 var albums = []album{
-    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
 // getAlbums responds with the list of all albums as JSON.
@@ -33,16 +34,16 @@ func getAlbums(c *gin.Context) {
 	// gin.Context.IndentedJSON serializes the struct into JSON and add it to the response
 	// To send more compact (unformatted) JSON, use Context.JSON
 	// http.StatusOK is the HTTP status code you want to send to the client
-    c.IndentedJSON(http.StatusOK, albums)
+	c.IndentedJSON(http.StatusOK, albums)
 }
 
 func addAlbum(c *gin.Context) {
-	
+
 }
 
 func main() {
-    router := gin.Default()
-    router.GET("/albums", getAlbums)
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
 
-    router.Run("localhost:8080")
-} 
+	router.Run("localhost:8080")
+}
