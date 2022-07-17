a60f6d4357d30aaa652f35262044ac54252d1f2b
James Ni <admacro@gmail.com>
2022-07-17 15:38:03 +0800

    data-access

---
+++ tut/data-access/main.go
@@ -0,0 +1,137 @@
+// https://go.dev/doc/tutorial/database-access
+// The data source name (DSN) is used by applications that need to access
+// or manage data in the database.
+// https://networkencyclopedia.com/data-source-name-dsn/
+//
+package main
+
+import (
+    "database/sql"
+    "fmt"
+    "log"
+    "os"
+
+    "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+type Album struct {
+	ID         int64
+	Title      string
+	Artist     string
+	Price      float32
+}
+
+func main() {
+	// Capture connection properties
+	cfg := mysql.Config{
+		User: os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net: "tcp",
+		Addr: "127.0.0.1:3306",
+		DBName: "recordings",
+		// AllowNativePasswords: true
+		// https://stackoverflow.com/questions/70757210/how-do-i-connect-to-a-mysql-instance-without-using-the-password
+		AllowNativePasswords: true,
+	}
+
+	// Get a database handle.
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Test the connection
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+
+	// Query for multiple rows
+	albums, err := albumsByArtist("John Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v\n", albums)
+
+	// Hard-code ID 2 here to test the query.
+	alb, err := albumByID(2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Album found: %v\n", alb)
+
+	// Add data
+	albID, err := addAlbum(Album{
+		Title:  "The Modern Sound of Betty Carter",
+		Artist: "Betty Carter",
+		Price:  49.99,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("ID of added album: %v\n", albID)
+}
+
+// albumsByArtist queries for albums that have the specified artist name.
+func albumsByArtist(name string) ([]Album, error) {
+    // An albums slice to hold data from returned rows.
+    var albums []Album
+
+    rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+    if err != nil {
+        return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+    }
+    defer rows.Close()
+    // Loop through rows, using Scan to assign column data to struct fields.
+    for rows.Next() {
+        var alb Album
+		// Scan takes a list of pointers to Go values, where the column
+		// values will be written. Here, you pass pointers to fields in the
+		// alb variable, created using the & operator. Scan writes through
+		// the pointers to update the struct fields.
+        if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+            return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+        }
+        albums = append(albums, alb)
+    }
+	// check for an error from the overall query, using rows.Err.
+	// Note that if the query itself fails, checking for an error here
+	// is the only way to find out that the results are incomplete.
+    if err := rows.Err(); err != nil {
+        return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+    }
+    return albums, nil
+}
+
+// albumByID queries for the album with the specified ID.
+func albumByID(id int64) (Album, error) {
+    // An album to hold data from the returned row.
+    var alb Album
+
+    row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
+    if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+        if err == sql.ErrNoRows {
+            return alb, fmt.Errorf("albumsById %d: no such album", id)
+        }
+        return alb, fmt.Errorf("albumsById %d: %v", id, err)
+    }
+    return alb, nil
+}
+
+// addAlbum adds the specified album to the database,
+// returning the album ID of the new entry
+func addAlbum(alb Album) (int64, error) {
+    result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES (?, ?, ?)", alb.Title, alb.Artist, alb.Price)
+    if err != nil {
+        return 0, fmt.Errorf("addAlbum: %v", err)
+    }
+    id, err := result.LastInsertId()
+    if err != nil {
+        return 0, fmt.Errorf("addAlbum: %v", err)
+    }
+    return id, nil
+}
