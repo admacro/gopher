000786c9d7e18c698b00eca428a53588bd90f6be
James Ni <admacro@gmail.com>
2022-07-23 23:13:15 +0800

    web service gin: post new album; get album by id;

--- tut/data-access/main.go
+++ tut/data-access/main.go
@@ -6,30 +6,30 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    "log"
-    "os"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
 
-    "github.com/go-sql-driver/mysql"
+	"github.com/go-sql-driver/mysql"
 )
 
 var db *sql.DB
 
 type Album struct {
-	ID         int64
-	Title      string
-	Artist     string
-	Price      float32
+	ID     int64
+	Title  string
+	Artist string
+	Price  float32
 }
 
 func main() {
 	// Capture connection properties
 	cfg := mysql.Config{
-		User: os.Getenv("DBUSER"),
+		User:   os.Getenv("DBUSER"),
 		Passwd: os.Getenv("DBPASS"),
-		Net: "tcp",
-		Addr: "127.0.0.1:3306",
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
 		DBName: "recordings",
 		// AllowNativePasswords: true
 		// https://stackoverflow.com/questions/70757210/how-do-i-connect-to-a-mysql-instance-without-using-the-password
@@ -78,60 +78,60 @@ func main() {
 
 // albumsByArtist queries for albums that have the specified artist name.
 func albumsByArtist(name string) ([]Album, error) {
-    // An albums slice to hold data from returned rows.
-    var albums []Album
+	// An albums slice to hold data from returned rows.
+	var albums []Album
 
-    rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
-    if err != nil {
-        return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
-    }
-    defer rows.Close()
-    // Loop through rows, using Scan to assign column data to struct fields.
-    for rows.Next() {
-        var alb Album
+	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	defer rows.Close()
+	// Loop through rows, using Scan to assign column data to struct fields.
+	for rows.Next() {
+		var alb Album
 		// Scan takes a list of pointers to Go values, where the column
 		// values will be written. Here, you pass pointers to fields in the
 		// alb variable, created using the & operator. Scan writes through
 		// the pointers to update the struct fields.
-        if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
-            return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
-        }
-        albums = append(albums, alb)
-    }
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
 	// check for an error from the overall query, using rows.Err.
 	// Note that if the query itself fails, checking for an error here
 	// is the only way to find out that the results are incomplete.
-    if err := rows.Err(); err != nil {
-        return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
-    }
-    return albums, nil
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	return albums, nil
 }
 
 // albumByID queries for the album with the specified ID.
 func albumByID(id int64) (Album, error) {
-    // An album to hold data from the returned row.
-    var alb Album
+	// An album to hold data from the returned row.
+	var alb Album
 
-    row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
-    if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
-        if err == sql.ErrNoRows {
-            return alb, fmt.Errorf("albumsById %d: no such album", id)
-        }
-        return alb, fmt.Errorf("albumsById %d: %v", id, err)
-    }
-    return alb, nil
+	row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		if err == sql.ErrNoRows {
+			return alb, fmt.Errorf("albumsById %d: no such album", id)
+		}
+		return alb, fmt.Errorf("albumsById %d: %v", id, err)
+	}
+	return alb, nil
 }
 
 // addAlbum adds the specified album to the database,
 // returning the album ID of the new entry
 func addAlbum(alb Album) (int64, error) {
-    result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES (?, ?, ?)", alb.Title, alb.Artist, alb.Price)
-    if err != nil {
-        return 0, fmt.Errorf("addAlbum: %v", err)
-    }
-    id, err := result.LastInsertId()
-    if err != nil {
-        return 0, fmt.Errorf("addAlbum: %v", err)
-    }
-    return id, nil
+	result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES (?, ?, ?)", alb.Title, alb.Artist, alb.Price)
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+	return id, nil
 }
--- tut/web-service-gin/main.go
+++ tut/web-service-gin/main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -37,13 +38,33 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
-func addAlbum(c *gin.Context) {
+func postAlbum(c *gin.Context) {
+	var newAlbum album
+	if err := c.BindJSON(&newAlbum); err != nil {
+		log.Fatalf("Invalid request body: %v")
+		return
+	}
+	newAlbum.ID = strconv.Itoa(len(albums) + 1)
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
 
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
+	router.POST("/albums", postAlbum)
 
 	router.Run("localhost:8080")
 }
