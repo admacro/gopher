8a49d3662619ccdbe994822ea306385398218b02
James Ni <admacro@gmail.com>
2022-07-24 16:24:42 +0800

    test with fuzz

---
+++ tut/fuzz/reverse_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"testing"
+	"unicode/utf8"
+)
+
+func TestReverse(t *testing.T) {
+	testcases := []struct {
+		in, want string
+	}{
+		{"Hello, world", "dlrow ,olleH"},
+		{" ", " "},
+		{"!12345", "54321!"},
+	}
+	for _, tc := range testcases {
+		r, err := Reverse(tc.in)
+		if r != tc.want {
+			t.Errorf("Reverse: %q, want: %q, err: %v", r, tc.want, err)
+		}
+	}
+}
+
+func FuzzReverse(f *testing.F) {
+	testcases := []string{"Hello, world", " ", "!12345"}
+	for _, tc := range testcases {
+		f.Add(tc) // Use f.Add to provide a seed corpus
+	}
+	f.Fuzz(func(t *testing.T, orig string) {
+		rev, revErr := Reverse(orig)
+		if revErr != nil {
+			return // skip the test if error
+		}
+		doubleRev, doubleRevErr := Reverse(rev)
+		if doubleRevErr != nil {
+			t.Skip() // skip the test if error
+		}
+		t.Logf("Number of runes: orig=%d, rev=%d, doubleRev=%d", utf8.RuneCountInString(orig), utf8.RuneCountInString(rev), utf8.RuneCountInString(doubleRev))
+		if orig != doubleRev {
+			t.Errorf("Before: %q, after: %q", orig, doubleRev)
+		}
+		if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+			t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+		}
+	})
+}
