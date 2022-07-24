13da5c8e1f501373f43fca50dce0ad8eb6b7bfd2
James Ni <admacro@gmail.com>
2022-07-24 17:16:28 +0800

    zero value and numeric constant

--- xah/numeric_constant.go
+++ xah/numeric_constant.go
@@ -1,13 +1,20 @@
+// https://go.dev/tour/basics/16
 package main
 
 import "fmt"
 
 func main() {
+	// Numeric constants are high-precision values.
 	// An untyped constant takes the type needed by its context.
 	const Base = 1000
+	const Rate = 0.1
 	fmt.Printf("%T\n", Base)     // int
 	fmt.Printf("%T\n", Base*2)   // int
-	fmt.Printf("%T\n", Base*0.1) // float64 (type depends on its context)
+	fmt.Printf("%T\n", Base*0.1) // float64 (type depends on its context, 0.1 is float64)
+
+	var i float32 = 0.1
+	fmt.Printf("%T\n", Rate*2) // float64 (type depends on its context, 0.1 is float64)
+	fmt.Printf("%T\n", Rate*i) // float32 (type depends on its context, i is float32)
 
 	var n = 1000
 	fmt.Printf("%T\n", n*2) // int
