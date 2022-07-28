42061b175f2c2aa5c78c3e744208b67426420da4
James Ni <admacro@gmail.com>
2022-07-28 23:33:31 +0800

    format code

--- tut/hello/hello.go
+++ tut/hello/hello.go
@@ -2,12 +2,15 @@
 // https://pkg.go.dev/rsc.io/quote/v4
 package main
 
-import "fmt"
-import "rsc.io/quote"
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
 
 func main() {
-    fmt.Println(quote.Hello())
-    fmt.Println(quote.Go())
-    fmt.Println(quote.Glass())
-    fmt.Println(quote.Opt())
+	fmt.Println(quote.Hello())
+	fmt.Println(quote.Go())
+	fmt.Println(quote.Glass())
+	fmt.Println(quote.Opt())
 }
